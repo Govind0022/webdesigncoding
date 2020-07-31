6dfdca6221655024ce9aa16136d4641dce58b87d
Govind0022 <68149947+Govind0022@users.noreply.github.com>
2020-07-31 16:35:49 +0530

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,49 @@
+var a=10;
+console.log(a)
+var aa='abc123'
+var ac="abc1234"
+console.log("aa values"+aa+"ac values"+ac)
+var ft=10.25
+console.log("float values"+ft)
+var z=true;
+var y=false;
+document.write("z values"+z+"y values"+y)
+_abc$=10
+document.write(_abc$)
+$abc=10
+document.write($abc)
+abc_123=10
+document.write(abc_123)
+//let
+let pi=3.14
+document.write(pi)
+	//identifiers
+	var hello;
+	document.write(hello)
+//mutiple variable
+var k='swami' n='gopi' a='anu' m='shaker' k='reddy' m='monoj' g='govind' s='sai'
+document.write(n+k+"students"+a+m+k+g+s)
+tt=4.25e+6;
+document.write(tt)
+tt1=4.25e-6;
+document.write(tt1)
+//alert('hello world')
+//confirm('hello world')
+//prompt('entermessage') // ok cancel enter
+hi="hellogood morning 'hello swami'"
+h1='hellogood morning "hello swami"'
+a=10
+b=20
+document.write(a<b);
+document.write(a>b);
+a=10;
+b=20;
+var c
+// alert(a<b);
+// alert(a>b);
+// alert(c);
+var tn-null;
+document.write(tn)
+//alert("hello good mrg" /2)
+//alert("hello good mrg" /2)
+//alert("hello good mrg" /2)
